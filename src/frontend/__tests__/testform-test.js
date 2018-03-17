cd65bb68b22f2aa2c429985c044219b11d0a311f
Jonathan Abantao <jonabantao@gmail.com>
2018-03-17 01:20:43 -0700

    Add babel-jest and setup enzyme

---
+++ src/frontend/__tests__/testform-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TestForm from '../testform';
+import { shallow } from 'enzyme';
+
+describe("TestForm", () => {
+  test("should handle form submit", () => {
+    expect().toEqual(2);
+  });
+});
