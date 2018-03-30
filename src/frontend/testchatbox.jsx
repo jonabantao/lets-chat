d1d6cbb22877a049a1f59541356781edd819d17c
Jonathan Abantao <jonabantao@gmail.com>
2018-03-30 15:24:05 -0700

    Add timestamps using momentjs

--- src/frontend/testchatbox.jsx
+++ src/frontend/testchatbox.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import moment from 'moment';
 
 import { receiveMessage } from '../util/socket';
 
@@ -26,14 +27,16 @@ class TestChatBox extends Component {
 
   render() {
     const chatMessages = this.state.messages.map((message, i) => {
+      const formattedTime = moment(message.createdAt).format('h:mm a');
+      
       return message.hasOwnProperty('url') ?
         (
           <li key={i}>
-            {message.from}: <a target="_blank" href={message.url}>My Current Location</a>
+            {message.from} {formattedTime}: <a target="_blank" href={message.url}>My Current Location</a>
           </li>
         ) :
         (
-          <li key={i}>{message.from}: {message.text}</li>
+          <li key={i}>{message.from} {formattedTime}: {message.text}</li>
         );
     });
     
