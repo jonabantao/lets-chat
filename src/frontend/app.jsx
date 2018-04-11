2437a95d14f31048d4759386afaa8b260f83d4c9
Jonathan Abantao <jonabantao@gmail.com>
2018-04-11 00:49:52 -0700

    Create a join page at the root page

--- src/frontend/app.jsx
+++ src/frontend/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import JoinPage from './components/join_page';
 import MainChat from './components/mainchat';
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={MainChat} />
+    <Route exact path="/" component={JoinPage} />
   </Switch>
 );
 
--- src/frontend/components/join_page.jsx
+++ src/frontend/components/join_page.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const JoinPage = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+class JoinPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      room: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  componentDidMount() {
+    document.title = 'Let\'s Chat | Join';
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // TODO: Handle form POST
+  }
+
+  handleUpdate(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <main className="centered-form">
+        <section className="centered-form__form">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-field">
+              <h3>Join Chat</h3>
+            </div>
+            <div className="form-field">
+              <h4>Display Name</h4>
+              <input type="text" value={this.state.name} onChange={this.handleUpdate} name="name" />
+            </div>
+            <div className="form-field">
+              <h4>Room Name</h4>
+              <input type="text" value={this.state.room} onChange={this.handleUpdate} name="room" />
+            </div>
+            <div className="form-field">
+              <button>Join</button>
+            </div>
+          </form>
+        </section>
+      </main>
+    );
+  }
+}
 
 export default JoinPage;
